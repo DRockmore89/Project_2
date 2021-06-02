103a73627f72dafa365702213195cc8f79bbc8d8
DRockmore89 <donaldrockmore@yahoo.com>
2021-06-01 21:19:22 -0400

    done2

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,12 +7,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/route-calculator", function(req, res) {
+    res.sendFile(path.join(__dirname,"../public/project2b.html/route-calculator"))
   });
 
   // Render 404 page for any unmatched routes
